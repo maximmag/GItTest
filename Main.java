71de58878a4a8a76a69bf90563636a8136a394d6
flegantovanura/ <flegantovanura@yandex.ru>
2019-07-01 14:42:39 +0300

    update main

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ public class Main {
     public void test1(){
         InputString inputString = new InputString();
 		InputString inputString2 = new InputString();
+		InputString inputString4 = new InputString();
 		InputString inputString3 = new InputString();
         String input = "Максим Магеррамов Человек кот кот мяукю.. Быаетт... ОДнако знайте, что Вологда лучшая, Вологда одна из лучшихФ!!";
         Assert.assertTrue(inputString.getString(input).equals("Магеррамов, Человек, Вологда, Вологда"));
